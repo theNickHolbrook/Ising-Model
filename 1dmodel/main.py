603e7e7307a1d008fe8b83958f4eeeade8116aa9
theNickHolbrook <nicholasjholbrook@gmail.com>
2026-04-03 12:46:45 -0400

    current_code

--- 1dmodel/main.py
+++ 1dmodel/main.py
@@ -1,3 +1,2 @@
-#this is for the ui and stuff
 
-
+        
--- 1dmodel/particles.py
+++ 1dmodel/particles.py
@@ -1,1 +1,18 @@
-#this is for the particles and stuff
+import random
+
+spins = [1,0,1,0,1,0,1,0]
+E = 0
+
+for i in spins:
+    E += i
+    
+
+for j in range(100):
+    for index, val in enumerate(spins):
+        if (E - val) < E:
+            spins[index] = 1 ^ val
+        else:
+            if random.random() <= 3 ** (-1 * E):
+                spins[index] = 1 ^ val
+
+    print(spins, j)
